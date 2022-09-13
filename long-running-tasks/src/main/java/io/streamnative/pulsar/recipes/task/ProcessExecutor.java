44f63698e409d9ddacd74559e64807e44989537a
teabot <elliot.west@streamnative.io>
2022-09-13 15:00:46 +0100

    Handle cancellation explicitly

--- long-running-tasks/src/main/java/io/streamnative/pulsar/recipes/task/ProcessExecutor.java
+++ long-running-tasks/src/main/java/io/streamnative/pulsar/recipes/task/ProcessExecutor.java
@@ -66,10 +66,10 @@ class ProcessExecutor<T, R> {
       }
       return future.get();
     } catch (CancellationException e) {
-      throw new ProcessException("Process was cancelled", e);
+      throw new ProcessException("Process was cancelled.", e);
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
-      throw new ProcessException("Task worker thread interrupted", e);
+      throw new ProcessException("Process was interrupted.", e);
     } catch (ExecutionException e) {
       throw new ProcessException("Processing error", e.getCause());
     } catch (Exception e) {
